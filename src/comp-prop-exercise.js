44df488a3f7412f1848c1b795844855f0aa72e88
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2018-09-15 10:02:38 +1200

    added shorthand, styling (class, style) dynamic and a couple of exerc.

---
+++ src/comp-prop-exercise.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: "#app",
+    data: {
+      value: 0
+    },
+    computed: {
+      result: function(){
+        return this.value == 37 ? 'done' : 'not there yet';
+      }
+    },
+    watch:{
+      result: function(){
+        var vm = this;
+        setTimeout(() => {
+          vm.value = 0;
+        }, 5000);
+      }
+    }
+  });
+  
