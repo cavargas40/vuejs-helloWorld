a8a2669566eca2f3298d5574a66698c9cfa1e55d
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2018-09-15 14:02:13 +1200

    added conditional and lists rendering

---
+++ src/rendering-lists.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: "#app",
+  data: {
+    ingredients: ['meat', 'fruit', 'cookies'],
+    persons: [
+        { name: 'Carlos', age: 26, color: 'red' },
+        { name: 'Xiomara', age: 'unknown', color: 'blue' }
+    ]
+  }
+});
